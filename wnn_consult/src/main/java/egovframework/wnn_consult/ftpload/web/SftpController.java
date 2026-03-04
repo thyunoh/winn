e33c09d2736f613501b2188b7e754b43ea9893d7
hyun.otech <hyun.otech@daum.net>
2026-03-05 01:18:48 +0900

    ..

--- wnn_consult/src/main/java/egovframework/wnn_consult/ftpload/web/SftpController.java
+++ wnn_consult/src/main/java/egovframework/wnn_consult/ftpload/web/SftpController.java
@@ -27,7 +27,7 @@ public class SftpController {
 
     // ✅ SFTP 연결 정보 (임시 하드코딩 – properties 방식 권장)
     
-    private static final String SFTP_HOST      = "114.108.153.178";
+    private static final String SFTP_HOST      = "localhost";// "114.108.153.178"; 
     private static final int    SFTP_PORT      = 22;
     private static final String SFTP_USER      = "winner";
     private static final String SFTP_PASSWORD  = "winner_20@%";
